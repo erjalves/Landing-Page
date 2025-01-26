fbe04af8013da0a5a9ce704cdb248d40f3d20c0e
erjalves <erjalves.eja@gmail.com>
2025-01-26 19:58:10 -0300

    first commit

---
+++ src/JavaScript/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $('.mobileBtn').on('click', function(){
+         $('#mobileMenu').toggleClass('active');
+         $('.mobileBtn').find('i').toggleClass('fa-x');
+
+    });
+
+    const sections = $('section');
+    const navItems = $('.itemMenu');
+
+    $(window).on('scroll',function(){
+       const header = $('header');
+       const scrollPosition = $(window).scrollTop() - header.outerHeight();
+       let activeSectionIndex = 0;
+
+     if(scrollPosition <= 0){
+          header.css('box-shadow','none');
+     }
+     else{
+          header.css('box-shadow', '5px, 1px, 5px rgba(0,0,0,0.1)');
+     }
+
+     sections.each(function(i){
+          const section = $(this);
+          const sectionTop = section.offset().top - 96;
+          const sectionBottom = sectionTop + section.outerHeight();
+
+          if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
+               activeSectionIndex = i;
+               return false;
+          };
+     });
+
+     navItems.removeClass('active');
+     $(navItems[activeSectionIndex]).addClass('active');
+    
+    });
+});  
